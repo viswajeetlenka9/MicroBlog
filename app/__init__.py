4b84b739ea1bbe4c1c2bf7cbd045fae38736fae3
Viswajeet Lenka <viswajeetlenka9@gmail.com>
2020-03-10 01:14:15 +0530

    Hello World,Templates,Web Forms,Database

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-app_flask = Flask(__name__)
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+#app.config['SECRET_KEY'] = 'you-will-never-guess-its-viswajeet'
 
-from app import routes
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template
-from app import app_flask
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm
 
-@app_flask.route('/')
-@app_flask.route('/index')
+@app.route('/')
+@app.route('/index')
 def index():
 	user = {'username': 'Viswajeet'}
 	posts = [
@@ -15,4 +16,12 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-	return render_template('index.html',title='Home',user=user)
+	return render_template('index.html',title='Home',user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+		return redirect(url_for('index'))
+	return render_template('login.html',title='Sign In',form=form)
