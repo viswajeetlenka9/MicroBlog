e0d3bc802a596fcb189bee969a3ecb09771ff5ac
Viswajeet Lenka <viswajeetlenka9@gmail.com>
2020-03-19 01:22:02 +0530

    Chapter Pagination

--- app/forms.py
+++ app/forms.py
@@ -43,6 +43,12 @@ class EditProfileForm(FlaskForm):
 				raise ValidationError('Please use a different username.')
 
 
+class PostForm(FlaskForm):
+	post = TextAreaField('Say something',validators=[
+		DataRequired(),Length(min=1, max=140)])
+	submit = SubmitField('Submit')
+
+
 
 
 
--- config.py
+++ config.py
@@ -17,3 +17,4 @@ class Config(object):
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	ADMINS = ['viswajeetlenka9@gmail.com']
+	POSTS_PER_PAGE = 3
